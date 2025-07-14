3635767a7879cf2178cf7c5dcdeb91f3f6fbc0e1
Karnpratik <karnpratik230@gmail.com>
2025-07-14 17:52:52 +0545

    new

--- terraform-project2/s3/main.tf
+++ terraform-project2/s3/main.tf
@@ -33,7 +33,10 @@ resource "aws_s3_bucket_policy" "policy" {
         "s3:PutObject",
         "s3:ListBucket"
       ],
-      "Resource": "${aws_s3_bucket.pratik_bucket.arn}/*"
+      Resource = [
+          "${aws_s3_bucket.pratik_bucket.arn}",
+          "${aws_s3_bucket.pratik_bucket.arn}/"
+      ]
     }
   ]
 }
