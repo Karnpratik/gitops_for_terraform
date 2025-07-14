9c8a4e2bfff84825e952f027b238de597c1b174f
Karnpratik <karnpratik230@gmail.com>
2025-07-14 09:51:38 +0545

    initial commit

---
+++ terraform-project2/s3/main.tf
@@ -0,0 +1,43 @@
+data "terraform_remote_state" "ec2" {
+  backend = "s3"
+  config = {
+    bucket = "pratik-bucketbackend"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_s3_bucket" "pratik_bucket" {
+  bucket = "com-pratik-bucket"
+
+  tags = {
+    Name = "pratikkarn-s3"
+    Creator = "pratikkarn"
+  }
+}
+
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.pratik_bucket.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "allowec2",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${data.terraform_remote_state.ec2.outputs.ec2_instance_arn}"
+      },
+      "Action": [
+        "s3:PutObject",
+        "s3:ListBucket"
+      ],
+      "Resource": "${aws_s3_bucket.pratik_bucket.arn}/*"
+    }
+  ]
+}
+POLICY
+}
+
+
