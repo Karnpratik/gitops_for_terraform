5c8043c60d9f6b23c8181ef0536080b19c0a40ac
Karnpratik <karnpratik230@gmail.com>
2025-07-14 17:42:53 +0545

    new

--- terraform-project2/s3/main.tf
+++ terraform-project2/s3/main.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "ec2" {
   backend = "s3"
   config = {
-    bucket = "pratik-bucketbackend"
+    bucket = "pratik-bucketbackend1"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
